131dec7bbf21cc646c2edf640020f83646405950
vibhas-pandey <pandey81730@gmail.com>
2026-04-16 06:55:17 +0530

    In demo added some examples

--- Demo/variable.js
+++ Demo/variable.js
@@ -7,4 +7,31 @@ let userPassword = "1234@vibhas"
 const isUserLoggedIn = false
 let userState;
 
-console.table([userId,userName,userEmail,userPassword,userState]);
+//console.table([userId,userName,userEmail,userPassword,userState]);
+
+/**
+ * How to create a object
+ * 
+ * why use object - because it groups multiple values in pair.
+ * using key value pair searching, readness, retrival. will be very much easy.
+ */
+
+const person = {
+    name : "vibhas",
+    age : 18,
+    address : "uttar pradesh"
+}
+//console.log(person);
+//console.log(person.address);
+
+/**
+ * Another way of grouping the element - Arraay
+ */
+
+const Person = ['vibhas', 18, 'true'];
+//console.log(Person);
+
+
+
+
+
