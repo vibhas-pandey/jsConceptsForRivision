96516fa81702835ae4ffc9d3e755abbbc9176adb
vibhas-pandey <pandey81730@gmail.com>
2026-04-16 06:39:35 +0530

    conversion of datatypes

---
+++ 01_Basics/03_conversionsOperations.js
@@ -0,0 +1,16 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+score = true
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+
