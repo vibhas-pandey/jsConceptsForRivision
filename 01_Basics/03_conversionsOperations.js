351f7d277112b5a3f1e61cb71850a0a59f8b9579
vibhas-pandey <pandey81730@gmail.com>
2026-04-20 07:13:32 +0530

    Operations added

--- 01_Basics/03_conversionsOperations.js
+++ 01_Basics/03_conversionsOperations.js
@@ -1,15 +1,45 @@
-let score = 33
+// let score = 33
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-score = true
-console.log(typeof score);
-console.log(typeof(score));
+// score = true
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//operations//
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2/2);
+//console.log(2 % 2);
+
+let value = 4
+let negValue = -value;
+//console.log(negValue);
+
+// console.log("2" + 1); sting to number concatenation always return string
+// console.log(1 + "2");
+
+let str1 = "hello";
+let str2 = "Vibhas";
+console.log((str1) + (str2));
+
+
+
+
+
+
+
+
+
 
 
 
